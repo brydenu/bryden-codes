665541f4e57a0eca4de89b85f25da1b5fc1901af
Bryden Uyehara <brydenuyehara@gmail.com>
2021-10-29 18:41:10 -0700

    App layout pretty much done, will have to finish actual routes and navbar/footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Routes from "./Routes";
+import { BrowserRouter } from "react-router-dom";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <NavBar />
+                <Routes />
+                <Footer />
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
