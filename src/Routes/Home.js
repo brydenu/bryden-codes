11ce8c0ed1fa16b362616b65ae26388b9f429501
brydenu <brydenu@gmail.com>
2022-09-23 11:31:04 -0700

    Firebase files + checking if autodeploy is working

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -4,6 +4,7 @@ const Home = () => {
     return (
         <div className="home">
             <h1>This is the homepage being rendered!</h1>
+            <p>I HOPE THIS AUTODEPLOYED!</p>
         </div>
     );
 };
