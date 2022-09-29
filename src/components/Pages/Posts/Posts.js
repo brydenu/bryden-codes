fa1a1cf7fe834df236906e6c5205be5b4b81c253
brydenu <brydenu@gmail.com>
2022-09-29 10:23:55 -0700

    Posts now show up. Early support for show more/less buttons

--- src/components/Pages/Posts/Posts.js
+++ src/components/Pages/Posts/Posts.js
@@ -11,19 +11,31 @@ export function Posts() {
     useEffect(() => (setShowing({"num": 5, posts: posts.slice(0, 5)})), [posts]);
 
     const handleShowMore = () => {
-        setShowing((current) => ({"num": current.num + 5, "posts": posts.slice(0, current.num + 5)}));
+        setShowing((current) => (
+            {"num": current.num + 5, "posts": posts.slice(0, current.num + 5)}
+            ));
+    }
+
+    const handleShowLess = () => {
+        const newNum = showing.num - 5 < 5 ? 5 : showing.num - 5;
+        setShowing(() => ({"num": newNum, "posts": posts.slice(0, newNum)}));
     }
 
     return (
         <div className="posts">
             <div className="posts-list">
-                {posts.slice(0, showing).map((post) => (
+                {showing.posts.map((post) => (
                     <PostPreview {...post} />
                 ))}
-                {posts.length > 
+                {posts.length > showing.num ? (
                 <div className="posts-list-show-btn" onClick={handleShowMore}>
                     Show More
                 </div>
+                ) : (
+                    <div className="posts-list-show-btn" onClick={handleShowLess}>
+                        Show Less
+                    </div>
+                )
                 }
             </div>
             <div className="posts-index">
